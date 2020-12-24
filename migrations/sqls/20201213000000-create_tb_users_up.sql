992d9b3c86f5301b6b0cbceb8f62013912239937
kaconk <harutonouciha@gmail.com>
2020-12-24 20:00:57 +0700

    delete migration

--- migrations/sqls/20201213000000-create_tb_users_up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS tb_users (
-    id UUID PRIMARY KEY,
-    nama VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(100) NOT NULL,
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP
-    );
