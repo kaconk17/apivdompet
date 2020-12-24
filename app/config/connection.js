f24de4f6b997239dd3e35e9aeb9c604f8a563fe7
kaconk <harutonouciha@gmail.com>
2020-12-24 21:28:22 +0700

    fix db url

--- app/config/connection.js
+++ app/config/connection.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 
 require('dotenv').config();
 
-const databaseConfig = { connectionString: process.env.DB_URL };
+const databaseConfig = { connectionString: process.env.DATABASE_URL };
 
 /*
 const pool = new Pool({
